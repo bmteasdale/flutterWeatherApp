36a9cd0d41f0f981c9b1074de14e1dd3907f17a8
Brendan Teasdale <brendanteasdale@Brendans-iMac.local>
2021-05-13 11:27:22 -0300

    Initial commit

---
+++ lib/models/weather_model.dart
@@ -0,0 +1,19 @@
+class Weather {
+  int temperature;
+  int minTemp;
+  int maxTemp;
+  int feelsLikeTemp;
+  String description;
+  double windSpeed;
+  int humidityPerc;
+
+  Weather({
+    this.temperature,
+    this.minTemp,
+    this.maxTemp,
+    this.feelsLikeTemp,
+    this.description,
+    this.windSpeed,
+    this.humidityPerc
+  });
+}
